03d933278ee04b959574549ee11796ecef454ed9
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2025-04-14 12:06:54 +0700

    menambahkan package validator dan membuat validasi

--- contacts.js
+++ contacts.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const chalk = require('chalk');
+var validator = require('validator');
 
 // Membuat Folder Data
 const dirPath = './data';
@@ -14,20 +16,38 @@ if (!fs.existsSync(filePath)) {
   console.log('File Path Berhasil Dibuat');
 }
 
-const simpanData = (nama, nomerHp, email) => {
-  const contact = { nama, nomerHp, email };
+const simpanData = (nama, email, nomerHp) => {
+  const contact = { nama, email, nomerHp };
   const fileBuffer = fs.readFileSync('./data/contacts.json', 'utf-8');
   const data = JSON.parse(fileBuffer);
-  data.push(contact);
 
-  const duplikat = data.find(({ nama }) => nama === nama);
-  if (duplikat) {
-    console.log('Contacts sudah terdaftar, gunakan nama lain');
-    return;
+  //Cek terlebih dahulu
+  const duplikat = data.find((element) => element.nama === nama);
+  if (duplikat !== undefined) {
+    console.log(`${chalk.red.inverse.bold('Contacts sudah terdaftar, gunakan nama lain')}`);
+    return false;
   }
 
+  // Cek Email
+  const emailValid = validator.isEmail(email);
+  // Mengecek lebih dulu kalo ada emailnya atau tidak karena konteks di project ini email tidak wajib di isi
+  if (email) {
+    if (!emailValid) {
+      console.log(chalk.red.inverse.bold('Format Email anda salah'));
+      return false;
+    }
+  }
+
+  // Cek Nomer HP
+  const nomer = validator.isMobilePhone(nomerHp, ['id-ID']);
+  if (!nomer) {
+    return console.log(chalk.red.inverse.bold('Format Nomer Salah'));
+  }
+
+  data.push(contact);
+
   fs.writeFileSync('./data/contacts.json', JSON.stringify(data, '', 2));
-  console.log('Berhasil di simpan');
+  console.log(chalk.green.bold('Contact Berhasil di simpan'));
 };
 
 module.exports = { simpanData };
